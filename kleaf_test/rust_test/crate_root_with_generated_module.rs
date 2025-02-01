5c174ab3c10d5218ba744b7e07358a0555e9c509
HONG Yifan <elsk@google.com>
2025-01-31 16:11:02 -0800

    kleaf: Add test for rust DDK module.
    Bug: 393475990
    Change-Id: Ic8bb44bb6df8c16c8b9c1ef22a8a09f38c003033

---
+++ kleaf_test/rust_test/crate_root_with_generated_module.rs
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: GPL-2.0
+//! Test of a standalone module with a single .rs file.
+
+use kernel::prelude::*;
+
+mod generated_module;
+use generated_module::module_fun;
+
+module! {
+    type: TestRustModule,
+    name: "test_rust_module",
+    author: "Hong, Yifan <elsk@google.com>",
+    description: "Test Rust module",
+    license: "GPL",
+}
+
+struct TestRustModule {}
+
+impl kernel::Module for TestRustModule {
+    fn init(_module: &'static ThisModule) -> Result<Self> {
+        module_fun();
+        Ok(TestRustModule {})
+    }
+}
