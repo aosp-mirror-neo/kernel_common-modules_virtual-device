fd391f59b428beff78e1598294c4cdf5ba29e5db
IOhannes m zmölnig <zmoelnig@umlautT.umlaeute.mur.at>
2025-10-07 23:36:37 +0200

    Yet another Linux-6.18 fix
    (cherry picked from commit ce8474158c6e70526cd799c64a079c32af057df4)
    Change-Id: I5d2ef34e52435077d533920dbcc0a9ea2657935b
    Signed-off-by: Carlos Llamas <cmllamas@google.com>

--- v4l2loopback/v4l2loopback.c
+++ v4l2loopback/v4l2loopback.c
@@ -57,6 +57,7 @@
 
 #if LINUX_VERSION_CODE < KERNEL_VERSION(6, 18, 0)
 #define v4l2_fh_add(fh, filp) v4l2_fh_add(fh)
+#define v4l2_fh_del(fh, filp) v4l2_fh_del(fh)
 #endif
 
 #define V4L2LOOPBACK_VERSION_CODE                                              \
@@ -2263,7 +2264,7 @@ static int v4l2_loopback_close(struct file *file)
 	}
 	try_free_buffers(dev);
 
-	v4l2_fh_del(&opener->fh);
+	v4l2_fh_del(&opener->fh, file);
 	v4l2_fh_exit(&opener->fh);
 
 	kfree(opener);
