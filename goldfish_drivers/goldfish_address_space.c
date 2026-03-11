mirfatif/android-virtual-device-kernel-modules

goldfish_address_space: add 16K support
Bug: 315325080
Test: boot emulator
Change-Id: Ia0c05b4b83ea567b7f0cfb939528d2d525b426e3
Signed-off-by: Roman Kiryanov <rkir@google.com>

goldfish_drivers/goldfish_address_space.c
@@ -15,6 +15,7 @@
 #include <linux/pci_regs.h>
 #include <linux/pci_ids.h>
 #include <linux/pci.h>
+#include <linux/page16.h>
 
 #include <goldfish/goldfish_address_space.h>
 
@@ -526,7 +527,7 @@ static int as_mmap(struct file *filp, struct vm_area_struct *vma)
 		&file_state->allocated_blocks;
 	struct as_allocated_blocks *shared_allocated_blocks =
 		&file_state->shared_allocated_blocks;
-	size_t size = PAGE_ALIGN(vma->vm_end - vma->vm_start);
+	size_t size = __PAGE_ALIGN(vma->vm_end - vma->vm_start);
 	int r;
 
 	WARN_ON(!allocated_blocks);
@@ -705,7 +706,7 @@ as_ioctl_ping_with_data_impl(struct as_ping_info_internal *ping_info,
 	if (copy_from_user(&user_copy, ptr, sizeof(user_copy)))
 		return -EFAULT;
 
-	if (user_copy.data_size > PAGE_SIZE - offsetof(struct goldfish_address_space_ping_with_data, data_ptr))
+	if (user_copy.data_size > __PAGE_SIZE - offsetof(struct goldfish_address_space_ping_with_data, data_ptr))
 		return -EFAULT;
 
 	ping_info->offset = user_copy.offset;
@@ -919,7 +920,7 @@ create_as_device(struct pci_dev *dev, const struct pci_device_id *id)
 
 	as_write_register(state->io_registers,
 			  AS_REGISTER_GUEST_PAGE_SIZE,
-			  PAGE_SIZE);
+			  __PAGE_SIZE);
 	as_write_register(state->io_registers,
 			  AS_REGISTER_PHYS_START_LOW,
 			  lower_32_bits(state->address_area_phys_address));

aosp-mirror-neo/kernel_common-modules_virtual-device

Bug: 487679661
Signed-off-by: Adrian Barnaś <abarnas@google.com>

goldfish_drivers/goldfish_address_space.c
@@ -15,6 +15,7 @@
 #include <linux/pci_regs.h>
 #include <linux/pci_ids.h>
 #include <linux/pci.h>
+#include <linux/page_size_compat.h>
 
 #include <goldfish/goldfish_address_space.h>
 
@@ -526,7 +527,7 @@ static int as_mmap(struct file *filp, struct vm_area_struct *vma)
 		&file_state->allocated_blocks;
 	struct as_allocated_blocks *shared_allocated_blocks =
 		&file_state->shared_allocated_blocks;
-	size_t size = PAGE_ALIGN(vma->vm_end - vma->vm_start);
+	size_t size = __PAGE_ALIGN(vma->vm_end - vma->vm_start);
 	int r;
 
 	WARN_ON(!allocated_blocks);
@@ -706,7 +707,7 @@ as_ioctl_ping_with_data_impl(struct as_ping_info_internal *ping_info,
 	if (copy_from_user(&user_copy, ptr, sizeof(user_copy)))
 		return -EFAULT;
 
-	if (user_copy.data_size > PAGE_SIZE - offsetof(struct goldfish_address_space_ping_with_data, data_ptr))
+	if (user_copy.data_size > __PAGE_SIZE - offsetof(struct goldfish_address_space_ping_with_data, data_ptr))
 		return -EFAULT;
 
 	ping_info->offset = user_copy.offset;
@@ -920,7 +921,7 @@ create_as_device(struct pci_dev *dev, const struct pci_device_id *id)
 
 	as_write_register(state->io_registers,
 			  AS_REGISTER_GUEST_PAGE_SIZE,
-			  PAGE_SIZE);
+			  __PAGE_SIZE);
 	as_write_register(state->io_registers,
 			  AS_REGISTER_PHYS_START_LOW,
 			  lower_32_bits(state->address_area_phys_address));